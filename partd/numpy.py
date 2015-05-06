fb3485b1688f7474c8b15d381dea912eabdd9fde
Matthew Rocklin <mrocklin@gmail.com>
2015-05-06 15:58:02 -0700

    add numpy

---
+++ partd/numpy.py
@@ -0,0 +1,31 @@
+from __future__ import absolute_import
+
+import numpy as np
+import struct
+from . import core
+
+
+def create(path, dtype):
+    """ Create store with known dtype """
+    core.create(path)
+    with open(core.filename(path, '.dtype'), 'w') as f:
+        f.write(str(dtype))
+
+
+def dtype(path):
+    with open(core.filename(path, '.dtype')) as f:
+        text = f.read()
+    return np.dtype(eval(text))  # TODO: this is insecure
+
+
+def put(path, data, put=core.put):
+    """ Put dict of numpy arrays into store """
+    bytes = dict((k, v.tobytes()) for k, v in data.items())
+    put(path, bytes)
+
+
+def get(path, keys, get=core.get):
+    """ Get list of numpy arrays from store """
+    bytes = get(path, keys)
+    dt = dtype(path)
+    return [np.frombuffer(bytes[i], dtype=dt[k]) for i, k in enumerate(keys)]
---
+++ partd/tests/test_numpy.py
@@ -0,0 +1,38 @@
+from __future__ import absolute_import
+
+from partd.numpy import create, put, get
+from partd import destroy, filename
+
+import numpy as np
+import os
+import shutil
+
+
+def test_partd():
+    path = 'tmp.partd'
+
+    if os.path.exists(path):
+        shutil.rmtree(path)
+
+    dt = np.dtype([('a', 'i4'), ('b', 'i2'), ('c', 'f8')])
+
+    assert not os.path.exists(path)
+    create(path, dt)
+    assert os.path.exists(path)
+
+    put(path, {'a': np.array([10, 20, 30], dtype=dt['a']),
+               'b': np.array([ 1,  2,  3], dtype=dt['b']),
+               'c': np.array([.1, .2, .3], dtype=dt['c'])})
+    put(path, {'a': np.array([70, 80, 90], dtype=dt['a']),
+               'b': np.array([ 7,  8,  9], dtype=dt['b']),
+               'c': np.array([.7, .8, .9], dtype=dt['c'])})
+    assert os.path.exists(filename(path, 'a'))
+    assert os.path.exists(filename(path, 'b'))
+    assert os.path.exists(filename(path, 'c'))
+
+    result = get(path, ['a', 'c'])
+    assert (result[0] == np.array([10, 20, 30, 70, 80, 90],dtype=dt['a'])).all()
+    assert (result[1] == np.array([.1, .2, .3, .7, .8, .9],dtype=dt['c'])).all()
+
+    destroy(path)
+    assert not os.path.exists(path)
