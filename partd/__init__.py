50288212cbc25b4018caefd067dc58df5bd6b788
Matthew Rocklin <mrocklin@gmail.com>
2015-05-18 16:00:08 -0700

    python init

--- partd/__init__.py
+++ partd/__init__.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 from .file import File
 from .encode import Encode
 from .pickle import Pickle
+from .python import Python
 from .compressed import *
 from .utils import ignoring
 with ignoring(ImportError):
