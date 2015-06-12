f45a0c7346ef841caaef859fa22d8e02e2a4c911
Matthew Rocklin <mrocklin@gmail.com>
2015-06-12 06:46:38 -0500

    add unicode to compat

--- partd/compatibility.py
+++ partd/compatibility.py
@@ -4,9 +4,11 @@ import sys
 
 if sys.version_info[0] == 3:
     from io import StringIO
+    unicode = str
     import pickle
     from queue import Queue, Empty
 if sys.version_info[0] == 2:
     from StringIO import StringIO
+    unicode = unicode
     import cPickle as pickle
     from Queue import Queue, Empty
