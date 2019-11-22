0cba0914797e91ea9e71b187b585402a3eef1cc6
Tom Augspurger <tom.w.augspurger@gmail.com>
2019-11-22 12:08:17 -0600

    fixed versioneer

--- partd/_version.py
+++ partd/_version.py
@@ -41,8 +41,8 @@ def get_config():
     cfg = VersioneerConfig()
     cfg.VCS = "git"
     cfg.style = "pep440"
-    cfg.tag_prefix = "None"
-    cfg.parentdir_prefix = "None"
+    cfg.tag_prefix = ""
+    cfg.parentdir_prefix = "partd-"
     cfg.versionfile_source = "partd/_version.py"
     cfg.verbose = False
     return cfg
