1ad35cf4f6ce3cedba4264641befd46b31299c6d
Thomas Grainger <tagrain@gmail.com>
2023-10-18 20:08:25 +0100

    install versioneer 0.29

--- partd/__init__.py
+++ partd/__init__.py
@@ -14,6 +14,5 @@ with suppress(ImportError):
 with suppress(ImportError):
     from .zmq import Client, Server
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
